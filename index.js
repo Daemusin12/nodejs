4caab5b65caec854ae2b3f0fcdc0b6340eb7bc3e
Daemusin12 <engrleonelfelezario812@gmail.com>
2023-06-07 15:43:57 +0800

    Module caching

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ const sum = add(1, 2);
 const sum2 = add(2, 3);
 console.log(sum);
 console.log(sum2);
+
+//Module caching
+const SuperHero = require("./super-hero");
+
+const batman = new SuperHero("Batman");
+console.log(batman.getName());
+batman.setName("Bruce Wayne");
+console.log(batman.getName());
+
+const superman = new SuperHero("Superman");
+console.log(superman.getName());
